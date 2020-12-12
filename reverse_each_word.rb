a445c8ec05744ec6d0083bdbfa229405763698ed
Ben Hayes <b.t.hayes@comcast.net>
2020-12-12 20:54:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,6 @@
 def reverse_each_word(argument)
-  final_array = []
   new_array = argument.split
-  new_array.each do |word|
-   final_array << word.reverse
+  new_array.collect do |word|
+   word.reverse
  end
- final_array.join(" ")
 end
